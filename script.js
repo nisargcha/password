de064387791cc16f84f65ba2f0072bad74349cc7
Nisarg Chaudhari <121618113+nisargcha@users.noreply.github.com>
2023-12-29 00:04:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function checkAnswer(){
+        let inputval = document.getElementById('input-box-text').value;
+        if(inputval === 'Nisvi'){
+            window.location.href = 'https://my-private-project.vercel.app/';
+        } else {
+            alert('Try Again Baby');
+        }    
+    }
+
